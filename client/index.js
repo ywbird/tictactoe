d3cc8e8befc566f8acbd383f7e7a5ee15e280fb6
ywbird <ywbird@naver.com>
2022-07-31 19:49:58 +0900

    heroku listen domain, port

--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 const gameElement = document.getElementById('gameElement');
 
-const socket = io('http://localhost:3000');
+const socket = io('https://fast-inlet-41976.herokuapp.com/');
 
 socket.on('init', handleInit);
 socket.on('gameState', handleGameState);
--- server/server.js
+++ server/server.js
@@ -94,4 +94,4 @@ function emitGameOver(room, winner) {
   io.to(room).emit('gameOver', JSON.stringify({ winner }));
 }
 
-io.listen(3000);
+io.listen(process.env.PORT || 3000);
